04de11a8a2c32d4e362706ecbaf35ee766e2ef1d
Oscar Mendoza <oscar.mendoza@nextiva.com>
2019-12-18 16:44:22 -0600

    Updates

---
+++ precache-manifest.5f99c0db9c91269032dc3e637abf3815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c0eb263e0ab869f5422d522dae7067",
+    "url": "/wavesurfer-demo/index.html"
+  },
+  {
+    "revision": "78e1001a00316bf8a432",
+    "url": "/wavesurfer-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f6b587f8f840faf87cd",
+    "url": "/wavesurfer-demo/static/js/2.60ae0188.chunk.js"
+  },
+  {
+    "revision": "b2c8fd7a6035cb0542a895c6f4d19948",
+    "url": "/wavesurfer-demo/static/js/2.60ae0188.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78e1001a00316bf8a432",
+    "url": "/wavesurfer-demo/static/js/main.bb057366.chunk.js"
+  },
+  {
+    "revision": "7150043b425f652bcb01",
+    "url": "/wavesurfer-demo/static/js/runtime-main.71d2824d.js"
+  }
+]);
